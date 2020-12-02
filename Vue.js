92e8eaf20f4d64a9f667ceaa2bce5b816f3ea03d
LHB153 <1075448489@qq.com>
2020-12-02 22:18:53 +0800

    It have the two characters, the one is data binding, the other is modularization which is let the message label show the h1 label

--- Vue.js
+++ Vue.js
@@ -4,3 +4,11 @@ var vm = new Vue({
         message : 'hello world, I am aec'
     }
 })
+var Message = Vue.extend({
+    props : ['content'],
+    template : '<h1>{{content}}</h1>'
+})
+Vue.component('message',Message);
+var vm1 = new Vue({
+    el: '#app2',
+})
